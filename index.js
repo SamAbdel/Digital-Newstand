3aa6337ce3b8ae4d61dbe9a42adf9372f751bc98
SamAbdel <119086762+SamAbdel@users.noreply.github.com>
2023-03-09 20:59:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function addp3(){
     }
 }
 function addp4(){
-    let LATimes = paper("rsc/LA.jpg", "LA Times", "5.99")
+    let LATimes = new paper("rsc/LA.jpg", "LA Times", "5.99")
     if(!Papers.some(check, LATimes)){
         Papers.push(LATimes) 
         alert('LA Times has been added to your cart')
@@ -57,7 +57,7 @@ function addp4(){
     }
 }
 function addp5(){
-    let WSJ = paper("rsc/WSJ.jpg", "Wall Street Journal", "6.99")
+    let WSJ = new paper("rsc/WSJ.jpg", "Wall Street Journal", "6.99")
     if(!Papers.some(check, WSJ)){
         Papers.push(WSJ) 
         alert('Wall Street Journal has been added to your cart')
@@ -68,7 +68,7 @@ function addp5(){
     }
 }
 function addp6(){
-    let CHI = paper("rsc/CHI.png", "Chicago Tribune", "3.99")
+    let CHI = new paper("rsc/CHI.png", "Chicago Tribune", "3.99")
     if(!Papers.some(check, CHI)){
         Papers.push(CHI) 
         alert('Chicago Tribune has been added to your cart')
@@ -81,5 +81,6 @@ function addp6(){
 }
 
 function store(){
-    sessionStorage.setItem("papers", JSON.stringify(Papers))
+    sessionStorage.setItem('papers', JSON.stringify(Papers))
 }
+
